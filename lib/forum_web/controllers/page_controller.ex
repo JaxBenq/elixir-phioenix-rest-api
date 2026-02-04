bf1e4f97b91374afd5458e2076a696dc362b147a
Josip Benko <josip.benko@hotmail.com>
2026-02-04 18:56:20 +0100

    Patch1

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -2,6 +2,14 @@ defmodule ForumWeb.PageController do
   use ForumWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    render(conn, :home, layout: false)
+  end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 2, name: "Bob", email: "bob@email.com"}
+    ]
+    json(conn, %{users: users})
   end
 end
